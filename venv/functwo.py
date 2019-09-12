3d3a9a06cec4eaca1ac8c0ff07cba78914a94e8f
Marc Silberkleit <marc@805stats.com>
2019-09-11 21:58:34 -0400

    more stuff2

--- venv/functwo.py
+++ venv/functwo.py
@@ -1,5 +1,6 @@
 import os
 from funcone import *
+import math
 
 print (addme(5,6,7,8,9,9))
 al= myclass()
@@ -12,3 +13,8 @@ print(os.path.join('C:\\opt','C:\\users\\marc','C:\\Programs'))
 print(os.path)
 for i in os.walk(os.getcwd()):
     print(i)
+print( dir(math))
+a=[100,110,130]
+b=[(-1,5)]
+print(sum(a))
+print(math.atan2(-1,2))
