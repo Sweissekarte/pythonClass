e8e7ebb09f47de689fc04393deae2a2db10f9d0a
Marc Silberkleit <51162834+Sweissekarte@users.noreply.github.com>
2019-09-12 10:06:52 -0400

    new files

--- venv/filetest.py
+++ venv/filetest.py
@@ -1,8 +1,8 @@
 import os
 
-#newfile = open('test.txt',"r")
-#newfile.write("Hi My name is harold")
-#print (newfile.read())
+newfile = open('test.txt',"r")
+newfile.write("Hi My name is harold")
+print (newfile.read())
 
 nums= set([11,2,3,3,3,4,4])
 print(len(nums))
