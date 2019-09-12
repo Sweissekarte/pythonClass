9cde823134853e052729ed0f959e25ad57bf8cb6
Marc Silberkleit <marc@805stats.com>
2019-09-11 21:37:43 -0400

    more stuff

--- venv/funcone.py
+++ venv/funcone.py
@@ -4,3 +4,11 @@ def addme(*nums):
     for i in nums:
         sum += i
     return sum
+
+class myclass():
+    def __init__(self):
+        self.__pri=("i am private")
+        self._pro=("i am protected")
+        self.pub=("i am public")
+
+
--- venv/functwo.py
+++ venv/functwo.py
@@ -1,4 +1,14 @@
 import os
-import funcone
+from funcone import *
 
-print (funcone.addme(5,6,7,8,9,9))
+print (addme(5,6,7,8,9,9))
+al= myclass()
+print(al.pub)
+print(al._pro)
+print(al._myclass__pri)
+print(os.name)
+print(os.getcwd())
+print(os.path.join('C:\\opt','C:\\users\\marc','C:\\Programs'))
+print(os.path)
+for i in os.walk(os.getcwd()):
+    print(i)
